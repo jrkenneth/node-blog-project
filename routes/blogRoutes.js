c28f5768c59bb45fbf9b3c41f4a1332464c9d90b
Ken's Macbook <Ken@Kens-MacBook-Pro.local>
2023-10-07 22:16:40 +0100

    Initial Commit

---
+++ routes/blogRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const blogController = require('../controllers/blogController');
+
+const router = express.Router();
+
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
+router.get('/update/:id', blogController.blog_update_get);
+router.patch('/update/:id', blogController.blog_update_post);
+
+module.exports = router;
